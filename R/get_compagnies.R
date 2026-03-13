03267e6267e71fc95cb54d85eb6ad1cfba536c30
YLB30 <yveslionel2000@yahoo.fr>
2026-03-13 14:50:33 +0100

    Check

--- R/get_compagnies.R
+++ R/get_compagnies.R
@@ -1,3 +1,41 @@
-con <- connect_db()
-DBI:: dbGetQuery(con, "SELECT * From adem.companies limit 100;")
-DBI::dbDisconnect(con)
+
+#'
+#'#' Get sample companies from ADEM database
+#'
+#' Retrieves the first 100 companies from the `adem.companies` table.
+#' Automatically establishes and closes the database connection.
+#'
+#' This is a convenience function for quick access to company data during
+#' development or exploration. Uses `connect_db()` internally and always
+#' closes the connection properly.
+#'
+#' @return A data frame with company data (up to 100 rows).
+#'
+#' @examples
+#' companies <- get_companies()
+#' head(companies)
+#' nrow(companies)
+#'
+#' @export
+#'
+#' @importFrom DBI dbGetQuery dbDisconnect
+get_companies <- function() {
+  con <- connect_db()
+  result <- DBI::dbGetQuery(con, "SELECT * FROM adem_companies LIMIT 100;")
+  DBI::dbDisconnect(con)
+  return(result)
+}
+
+#' @returns
+#' @export
+#'
+#' @examples
+get < - function(){
+
+  con <- connect_db()
+  DBI:: dbGetQuery(con, "SELECT * From adem.companies limit 100;")
+  DBI::dbDisconnect(con)
+
+}
+
+
